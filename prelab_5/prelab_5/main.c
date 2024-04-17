9c2a4ecc359c10629f401d3c12c05fd5a76e6f35
nliefaj <faj22060@uvg.edu.gt>
2024-04-17 12:14:08 -0600

    Terminado

---
+++ prelab_5/prelab_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * prelab_5.c
+ *
+ * Created: 4/16/2024 9:52:28 PM
+ * Author : lefaj
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
